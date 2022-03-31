444c994edf224a42a693a16019c27647b7c1e535
Hatidza97 <hatidza.bojcic@edu.fit.ba>
2022-03-31 17:31:20 +0200

    .

---
+++ eCabinRental/eCabinRental/Controllers/KorisniciController.cs
@@ -0,0 +1,25 @@
+﻿using eCabinRental.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCabinRental.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KorisniciController:ControllerBase
+    {
+        private readonly IKorisniciService _service;
+        public KorisniciController(IKorisniciService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public IList<Korisnik> Get()
+        {
+            return _service.Get();
+        }
+    }
+}
---
+++ eCabinRental/eCabinRental/Services/KorisniciService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCabinRental.Services
+{
+    public class KorisniciService: IKorisniciService
+    {
+        public BSContext context { get; set; }
+        public KorisniciService(BSContext Context)
+        {
+            context = Context;
+        }
+        public List<Korisnik> Get()
+        {
+            return context.Korisniks.ToList();
+
+        }
+    }
+}
